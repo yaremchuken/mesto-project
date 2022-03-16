e113eae977eeb4843073a0bc646ab220222f6f17
yaremchuken <yaremchuken@yandex-team.ru>
2022-03-16 19:32:48 +0300

    Fix selector naming

--- src/components/Section.js
+++ src/components/Section.js
@@ -2,13 +2,13 @@
 export default class Section {
   constructor({ renderer }, containerSelector) {
     this._renderer = renderer;
-    this._containerSelector = document.querySelector(containerSelector);
+    this._containerElement = document.querySelector(containerSelector);
   }
 
   // Публичная функция для "ручного" добавления карточки
   addItem = (card) => {
     const generated = card.generateCard();
-    this._renderer(generated, this._containerSelector);
+    this._renderer(generated, this._containerElement);
   };
 
   // Публичная функция для отрисовки массива карточек, которые придут с сервера. А они уже последовательно будут передаваться в контейнер через функцию addItem()
